2af680f6e5b22629d49158c40fca790565d3846f
joyjoysan <joyjoysan@users.noreply.github.com>
2021-10-27 17:40:53 -0500

    Some UI work on "Create User"

--- src/screens/CreateUserScreen/CreateUserScreen.js
+++ src/screens/CreateUserScreen/CreateUserScreen.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import style from './style';
 import { StyleSheet, Text,SafeAreaView, View, Button, TouchableOpacity } from 'react-native'
+import { Input, Datepicker, Icon, Layout } from '@ui-kitten/components';
+
+const CalendarIcon = (props) => (
+    <Icon {...props} name='calendar'/>
+  );
 
 export default function CreateUserScreen({navigation}) {
     const onLogout = () => {
@@ -9,16 +14,58 @@ export default function CreateUserScreen({navigation}) {
     const onSubmit = () => {
         navigation.navigate('Dashboard')
     }
+    const [value, setValue] = React.useState('');
+    const [date, setDate] = React.useState(new Date());
     return (
         <View style={style.container}>
-            <Text>Create User Profile</Text>
+            <View style={style.inputView}>
+                <Input
+                    label = 'First Name'
+                    placeholder='Place your Text'
+                    value={value}
+                    onChangeText={nextValue => setValue(nextValue)}
+                />
+            </View>
+            <View style={style.inputView}>
+                <Input
+                    label = 'Last Name'
+                    placeholder='Place your Text'
+                    value={value}
+                    onChangeText={nextValue => setValue(nextValue)}
+                />
+            </View>
+            <Datepicker
+                label='Birthday'
+                placeholder='Pick Date'
+                date={date}
+                onSelect={nextDate => setDate(nextDate)}
+                accessoryRight={CalendarIcon}
+            />
+            <View style={style.inputView}>
+                <Input
+                    label = 'Birthday'
+                    placeholder='Place your Text'
+                    value={value}
+                    onChangeText={nextValue => setValue(nextValue)}
+                />
+            </View>
             <TouchableOpacity style={style.logoutBtn} onPress={onSubmit}>
                 <Text>Submit</Text>
             </TouchableOpacity>   
-            <TouchableOpacity style={style.logoutBtn} onPress={onLogout}>
-                <Text>BACK</Text>
-            </TouchableOpacity>
         </View>
         
     )
 }
+/*
+First Name
+Last Name
+Email
+KU ID
+Sexual Orientation
+Gender
+Age
+Location
+Dating Preferences (Age, Age Range)
+Top 5 matches and relevant matches afterwards
+Personalized User questions/answers
+*/
--- src/screens/CreateUserScreen/style.js
+++ src/screens/CreateUserScreen/style.js
@@ -13,4 +13,18 @@ export default StyleSheet.create({
         marginTop: 40,
         backgroundColor: "#FF1493",
     },
+    TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+    },
+    inputView: {
+        backgroundColor: "#FFC0CB",
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
+        marginTop: 20,
+        alignItems: "center",
+    },
 })
