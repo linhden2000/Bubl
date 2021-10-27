79b245f5c262daed62e841a8ba3810284370baa3
joyjoysan <joyjoysan@users.noreply.github.com>
2021-10-27 17:40:31 -0500

    add kitten ui dependencies

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
+import * as eva from '@eva-design/eva';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import { LoginScreen, RegistrationScreen, DashboardScreen, CreateUserScreen} from "./src/screens";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  
   return (
+    <ApplicationProvider {...eva} theme={eva.light}>
+    <IconRegistry icons={EvaIconsPack} />
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: true}}>
         <Stack.Screen name="Login" component={LoginScreen}/>
@@ -17,6 +23,8 @@ export default function App() {
         <Stack.Screen name="Create User" component={CreateUserScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
+    </ApplicationProvider>
+    
   );
 }
 
