abe0f511c5790c60676baff8d726510fad51e557
Linh <linh.ng2000@gmail.com>
2021-11-09 00:24:56 -0600

    fixing nav bar

--- src/screens/MessageScreen/MessageScreen.js
+++ src/screens/MessageScreen/MessageScreen.js
@@ -1,26 +1,11 @@
 import React from 'react'
 import style from './style';
 import { StyleSheet, Text,SafeAreaView, View, TouchableOpacity } from 'react-native'
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faHome, faComment, faUser} from '@fortawesome/free-solid-svg-icons'
 
 export default function MessageScreen({navigation}) {
     const onLogout = () => {
         navigation.navigate('Login')
     }
-    
-    const onMessage = () => {
-        navigation.navigate('Message')
-    }
-
-    const onProfile = () => {
-        navigation.navigate('Profile')
-    }
-
-    const onDashBoard = () => {
-        navigation.navigate('Dashboard')
-    }
-    
 
     return (
         <View style={style.container}>
@@ -29,20 +14,7 @@ export default function MessageScreen({navigation}) {
             <TouchableOpacity style={style.logoutBtn} onPress={onLogout}>
               <Text>LOGOUT</Text>
             </TouchableOpacity> 
-            
-            <View style={style.navbar}>
-              <TouchableOpacity style={style.messageBtn} onPress={onMessage}>
-                <FontAwesomeIcon icon={faComment} />
-              </TouchableOpacity> 
-
-              <TouchableOpacity style={style.homeBtn} onPress={onDashBoard}>
-                <FontAwesomeIcon icon={faHome} />
-              </TouchableOpacity> 
-
-              <TouchableOpacity style={style.profileBtn} onPress={onProfile}>
-                <FontAwesomeIcon icon={faUser} />
-              </TouchableOpacity> 
-            </View>       
+                   
         </View>
     )
 }
--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -1,37 +1,11 @@
 import React from 'react'
 import style from './style';
 import { Image, StyleSheet, Text,SafeAreaView, View, Button, TouchableOpacity } from 'react-native'
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faHome, faComment, faUser} from '@fortawesome/free-solid-svg-icons'
 
 
 export default function ProfileScreen({navigation}) {
     const onLogout = () => {
         navigation.navigate('Login')
-    }
-    
-    const onMessage = () => {
-        navigation.navigate('Message')
-    }
-
-    const onProfile = () => {
-        navigation.navigate('Profile')
-    }
-
-    const onDashBoard = () => {
-        navigation.navigate('Dashboard')
-    }
-
-    const onMyProfile = () => {
-        navigation.navigate('MyProfile')
-    }
-
-    const onPreference = () => {
-      navigation.navigate('Preference')
-    }
-
-    const onSettings = () => {
-      navigation.navigate('AccountSettings')
     }
     
     return (
@@ -44,13 +18,13 @@ export default function ProfileScreen({navigation}) {
         <Text style={{left: 45, fontSize: 15}}>Looking for my Fiona</Text>
           
         <View style={style.content}>
-          <TouchableOpacity style={style.Btn} onPress={onMyProfile}>
+          <TouchableOpacity style={style.Btn}>
             <Text style={style.text}>My Profile</Text>
           </TouchableOpacity> 
-          <TouchableOpacity style={style.Btn} onPress={onPreference}>
+          <TouchableOpacity style={style.Btn}>
             <Text style={style.text}>Preference</Text>
           </TouchableOpacity> 
-          <TouchableOpacity style={style.Btn} onPress={onSettings}>
+          <TouchableOpacity style={style.Btn}>
             <Text style={style.text}>Account Settings</Text>
           </TouchableOpacity> 
           <TouchableOpacity style={style.Btn}>
@@ -63,21 +37,7 @@ export default function ProfileScreen({navigation}) {
           <TouchableOpacity onPress={onLogout}>
             <Text>LOGOUT</Text>
           </TouchableOpacity>
-        </View>
-            
-        <View style={style.navbar}>
-          <TouchableOpacity style={style.messageBtn} onPress={onMessage}>
-            <FontAwesomeIcon icon={faComment} />
-          </TouchableOpacity> 
-
-          <TouchableOpacity style={style.homeBtn} onPress={onDashBoard}>
-            <FontAwesomeIcon icon={faHome} />
-          </TouchableOpacity> 
-
-          <TouchableOpacity style={style.profileBtn} onPress={onProfile}>
-              <FontAwesomeIcon icon={faUser} />
-          </TouchableOpacity> 
-        </View>        
+        </View>     
       </View>
     )
 }
