e281d82cb6db54dfd86333f9d2920c6a30ed7114
linhden2000 <linh.ng2000@gmail.com>
2021-11-21 17:35:23 -0600

    UI Login

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,10 +1,20 @@
 import React, {useEffect, useState} from 'react'
 import style from './style';
-import { StyleSheet, Text, TextInput, SafeAreaView, View, Button, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, TextInput, SafeAreaView, View, Button, TouchableOpacity, Image, ImageBackground } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons';
+import logo from '../../../assets/bublLogo.png';
+
+import AppLoading from 'expo-app-loading';
+//import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import {
+    useFonts,
+    OleoScript_400Regular,
+    OleoScript_700Bold,
+  } from '@expo-google-fonts/oleo-script';
+
 import { auth } from '../../firebase/config';
 import * as Animatable from 'react-native-animatable';
 
- 
 
 export default function LoginScreen({navigation}) {
     const [email, setEmail] = useState('')
@@ -59,48 +69,89 @@ export default function LoginScreen({navigation}) {
             setValidPassword(false)
         }
     }
-    
 
-    return (
-        <View style={style.container}>
-            <View style={style.inputView}>
-                <TextInput
-                    style={style.TextInput}
-                    placeholder="Email"
-                    placeholderTextColor="#003f5c" 
-                    onChangeText={text=>setEmail(text)}
-                    onEndEditing={text=>handleEmailChange(text.nativeEvent.text)}
-                />
+    let [fontsLoaded] = useFonts({
+        OleoScript_400Regular,
+        OleoScript_700Bold,
+    });
+
+    if(!fontsLoaded) {
+        return <AppLoading />;
+    }
+    else {
+        return (
+            <View style={style.container}>
+                <ImageBackground style={style.imageBG} resizeMode="cover" source={require("../../../assets/bg.png")} />
+                <View style={style.box}>
+                    <View style={style.logo}>
+                        <Image style= {{ alignSelf: "center", width: 180, height: 180}} source={logo} />
+                        <Text style={{marginTop: -90, alignSelf: "center",color:"#8898AA", fontFamily: "OleoScript_400Regular", fontSize: 30 }}>Bubl</Text>
+                    </View>
+                    
+                    
+                    <View style={style.social}>
+                        {/* <Text style={{textAlignVertical: "center",textAlign: "center", fontSize: "15", color:"#8898AA"}}> Log in with</Text> */}
+                        <View style={style.socialElement}>
+                            <FontAwesome.Button name="facebook" backgroundColor="#3b5998" justifyContent="center">Login with Facebook </FontAwesome.Button>
+                        </View>
+                        <View style={style.socialElement}>
+                            <FontAwesome.Button name="google" backgroundColor="#DB4437" justifyContent="center">Login with Google </FontAwesome.Button>
+                        </View> 
+                    </View>
+    
+                    <View style={style.login}>
+                        <Text style={{marginTop: 30, textAlignVertical: "center",textAlign: "center", fontSize:15, color:"#8898AA"}}> Or log in the classic way</Text>
+                        <View style={style.inputView}>
+                            <View style={{textAlign: 'center',alignItems:"center", justifyContent: "center",fontSize: 15, width: 45, height: 45}} >
+                                <FontAwesome name="envelope"/>
+                            </View>
+                            <TextInput
+                                style={style.TextInput}
+                                placeholder="Email"
+                                placeholderTextColor="#003f5c" 
+                                onChangeText={text=>setEmail(text)}
+                                onEndEditing={text=>handleEmailChange(text.nativeEvent.text)}
+                            />
+                        </View>
+                        { isValidEmail ? null : 
+                        <Animatable.View animation="fadeInLeft" duration={500}>
+                        <Text style={style.errorMsg}>Email is required .</Text>
+                        </Animatable.View>
+                        }
+                        <View style={style.inputView}>
+                            <View style={{textAlign: 'center',alignItems:"center", justifyContent: "center",fontSize: 15, width: 45, height: 45}} >
+                                <FontAwesome name="unlock-alt"/>
+                            </View>
+                            <TextInput
+                                style={style.TextInput}
+                                placeholder="Password" secureTextEntry
+                                placeholderTextColor="#003f5c"
+                                onChangeText={text=>setPassword(text)}
+                                onEndEditing={text=>handlePasswordChange(text.nativeEvent.text)}
+                            />
+                        </View>
+                        { isValidPassword ? null : 
+                        <Animatable.View animation="fadeInLeft" duration={500}>
+                        <Text style={style.errorMsg}>Password is required .</Text>
+                        </Animatable.View>
+                        }
+                        <TouchableOpacity>
+                            <Text style={{marginTop: 30, textAlignVertical: "center",textAlign: "center", fontSize:15, color:"#8898AA"}} onPress={onLogin}>Forgot Password?</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={style.loginBtn} onPress={onLogin}>
+                            <Text >LOGIN</Text>
+                        </TouchableOpacity>
+                        <View style={{ flexDirection: "row", justifyContent: "center"}}>
+                            <Text style={{marginTop: 30, textAlignVertical: "center",textAlign: "center", fontSize:15, color:"#8898AA"}}> Don't have account? </Text>
+                            <TouchableOpacity>
+                                <Text style={{marginTop: 30, textAlignVertical: "center",textAlign: "center", fontSize:15}} onPress={onRegistration}>Sign up</Text>
+                            </TouchableOpacity>
+                        </View>
+                        </View>
+                </View>    
             </View>
-            { isValidEmail ? null : 
-            <Animatable.View animation="fadeInLeft" duration={500}>
-            <Text style={style.errorMsg}>Email is required .</Text>
-            </Animatable.View>
-            }
-            <View style={style.inputView}>
-                <TextInput
-                    style={style.TextInput}
-                    placeholder="Password" secureTextEntry
-                    placeholderTextColor="#003f5c"
-                    onChangeText={text=>setPassword(text)}
-                    onEndEditing={text=>handlePasswordChange(text.nativeEvent.text)}
-                />
-            </View>
-            { isValidPassword ? null : 
-            <Animatable.View animation="fadeInLeft" duration={500}>
-            <Text style={style.errorMsg}>Password is required .</Text>
-            </Animatable.View>
-            }
-            <TouchableOpacity>
-                <Text style={style.forgot_button} onPress={onLogin}>Forgot Password?</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={style.loginBtn} onPress={onLogin}>
-                <Text style={style.loginText}>LOGIN</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Text style={style.forgot_button} onPress={onRegistration}>Don't have account? Sign up</Text>
-            </TouchableOpacity>
-        </View>
-    )
+        )
+    }
+    
 }
 
--- src/screens/LoginScreen/style.js
+++ src/screens/LoginScreen/style.js
@@ -1,44 +1,91 @@
 // style for Login goes here
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get("screen");
+
 export default StyleSheet.create({
 
     container: {
         flex: 1,
-        backgroundColor: "#fff",
+        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
     },
+    imageBG:{
+        flex: 1,
+        justifyContent:"center",
+        position: "absolute",
+        width:width,
+        height:height
+    },
+    box:{
+        flex: 1,
+        alignContent:"center",
+        justifyContent: "space-between",
+        position: "absolute",
+        backgroundColor:"#FFFFFF",
+        width: width * 0.9,
+        height: height * 0.8,
+        borderRadius: 20,
+    },
+    logo:{
+        flex: 1,
+        width: width * 0.9,
+        height: height * 0.2,
+    },
+    social:{
+        flex: 2,
+        alignSelf:"center",
+        justifyContent: "center",
+        borderColor: "#8898AA",
+        width: width * 0.9, 
+        height: height* 0.5,
+        borderRadius: 20,
+    },
+    socialElement:{
+        marginTop: 20,
+        alignSelf:"center",
+        justifyContent: "center",
+        width: width / 1.25, 
+    },
+
+    login:{
+        backgroundColor: "#F4F5F7",
+        //alignSelf:"center",
+        alignContent:"flex-end",
+        //justifyContent: "flex-end",
+        width: width * 0.9, 
+        height: height * 0.5,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+    },
 
     inputView: {
-        backgroundColor: "#FFC0CB",
-        borderRadius: 30,
-        width: "70%",
+        backgroundColor: "#FFFFFF",
+        borderRadius: 10,
+        width: "80%",
         height: 45,
-        marginBottom: 20,
-     
-        alignItems: "center",
+        marginTop: 30,
+        alignSelf:"center",
+        flexDirection: "row",
       },
 
     TextInput: {
         height: 50,
         flex: 1,
-        padding: 10,
-        marginLeft: 20,
-    },
-
-    forgot_button: {
-        height: 30,
-        marginBottom: 30,
+        alignSelf:"center",
+        //padding: 10,
     },
 
     loginBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#FF1493",
+        width: "80%",
+        borderRadius: 10,
+        height: 50,
+        alignItems: "center",
+        alignSelf:"center",
+        justifyContent: "center",
+        marginTop: 30,
+        backgroundColor: '#5E72E4',
   },
   
   errorMsg: {
